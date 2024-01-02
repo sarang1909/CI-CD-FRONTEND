e9aede110b7e7bf31dacbaf7c26b0d49cc8cec9d
sarang1909 <56933093+sarang1909@users.noreply.github.com>
2024-01-02 18:36:13 -0500

    seconf commit

---
+++ src/PostCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async event => {
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+
+        setTitle('');
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className = "form-group">
+                    <label>Title</label>
+                    <input 
+                        className="form-control" 
+                        value={title} 
+                        type="text" 
+                        onChange={ e => setTitle(e.target.value)}/>
+                </div>
+                <br />
+                <button className="btn btn-primary">SUBMIT</button>
+            </form>
+        </div>
+    );
+};
+
+export default PostCreate;
